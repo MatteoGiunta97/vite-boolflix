e7547e93a88e8d5efa242249b503ac5eb912e4ba
MatteoGiunta97 <matteogiunta@outlook.it>
2024-04-18 13:58:53 +0200

    feat: add ContentList and Card components

---
+++ src/components/Card.vue
@@ -0,0 +1,41 @@
+<script>
+  export default {
+      name: 'Card',
+  }
+</script>
+
+<template>
+
+    <div class="card">
+        <div class="info-wrapper">
+            <div class="info">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit molestias voluptatum asperiores enim accusamus, illum ipsam quibusdam adipisci quos commodi fuga tempora saepe quis beatae magnam hic iusto quas perspiciatis?</p>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+
+.card {
+    width: calc(100% / 3);
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    
+    .info-wrapper {
+        background-color: black;
+        text-align: center;
+        padding: 10px;
+        width: 100%;
+        height: 100%;
+
+        p {
+            color:white;
+        }
+    }
+}
+
+</style>
--- src/components/ContentList.vue
+++ src/components/ContentList.vue
@@ -1,14 +1,42 @@
 <script>
+  import { store } from "../store.js";
+  import Card from "./Card.vue";
+
   export default {
-      name: 'ContentList'
+      name: 'ContentList',
+        components: {
+          Card
+        }
+        ,
+        data() {
+          return {
+            store
+          };
+        }
   }
 </script>
 
 <template>
-    
+  
+  <section class="cards-wrapper">
+    <div class="container">
+      <div class="cards-list">
+        <Card></Card>
+      </div>
+    </div>
+  </section>
+
 </template>
 
 <style scoped lang="scss">
 
+.cards-wrapper {
+  background-color: grey;
+  padding: 20px;
 
+    .cards-list {
+        display: flex;
+        flex-wrap: wrap;
+    }
+}
 </style>

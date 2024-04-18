faedcfbf40bd083b5a02b7c2c105623f972945dc
MatteoGiunta97 <matteogiunta@outlook.it>
2024-04-18 13:31:05 +0200

    feat: add scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <script>
+  import axios from 'axios';
+  import { store } from "./store.js";
+  import AppHeader from './components/AppHeader.vue';
+  import ContentList from './components/ContentList.vue';
+
+  export default {
+    components: {
+      AppHeader,
+      ContentList
+    }
+    ,
+    data() {
+      return {
+        store
+      };
+    }
+    ,
+    methods: {
+      
+    }
+    ,
+    mounted() {
+
+    }
+    }
 </script>
 
 <template>
 
 </template>
 
-<style>
-
+<style lang="scss">
+@use './style/generic';
 </style>
